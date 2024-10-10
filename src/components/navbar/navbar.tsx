f6073d2ccca836733917f6f739f235d3aeacb48d
Akhil Vinnakota <AkhilVinnakota@users.noreply.gitlab.com>
2024-10-10 21:27:44 +0530

    feat: implement onHover service subItems and added searchParams

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -1,6 +1,9 @@
+import { useState } from 'react';
 import { NavLink } from 'react-router-dom';
+import ServiceMenuItems from './service-menu-items';
 
 export default function NavbarComponent() {
+  const [dropdown, setDropdown] = useState<boolean>(false);
   const navItems = [
     {
       id: 1,
@@ -20,20 +23,27 @@ export default function NavbarComponent() {
   ];
 
   return (
-    <header className="flex justify-between bg-[#E8F5F3] p-3 px-12">
+    <header className="flex justify-between bg-[#E8F5F3] p-3 px-12 relative">
       <img width="10%" height="10%" className="ml-3" src="./logo.svg" alt="logo" />
       <ul className="flex justify-around items-center [&>*]:mx-11 font-poppins font-semibold">
         {navItems.map(item => {
           return (
-            <NavLink
+            <li
               key={item.id}
-              to={item.path}
-              className={({ isActive, isPending }) =>
-                isPending ? 'pending' : isActive ? 'underline decoration-[3px] decoration-[#69D7C1] underline-offset-8' : ''
-              }
+              className="flex items-center justify-center"
+              onMouseEnter={item.item === 'Service' ? () => setDropdown(true) : undefined}
+              onMouseLeave={item.item === 'Service' ? () => setDropdown(false) : undefined}
             >
-              {item.item}
-            </NavLink>
+              <NavLink
+                className={({ isActive, isPending }) =>
+                  isPending ? 'pending' : isActive ? 'underline decoration-[3px] decoration-[#69D7C1] underline-offset-8 h-full' : ''
+                }
+                to={item.path}
+              >
+                {item.item}
+              </NavLink>
+              {item.item === 'Service' && dropdown && <ServiceMenuItems />}
+            </li>
           );
         })}
       </ul>
